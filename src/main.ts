82eb780f8cd9a41c36e88212cdf119275e5cf422
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-08 16:19:05 -0300

    feat: add support for swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Lumio API')
+    .setDescription('Documentação interativa da API do Lumio')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
