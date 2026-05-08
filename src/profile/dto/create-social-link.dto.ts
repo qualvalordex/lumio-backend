f6f250925e27c1efc2d5f586d6ef5252d21a5c90
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-08 20:26:21 -0300

    feat: add api property to ensure swagger docs

--- src/profile/dto/create-social-link.dto.ts
+++ src/profile/dto/create-social-link.dto.ts
@@ -1,10 +1,13 @@
 import { IsEnum, IsString } from 'class-validator';
 import { SocialLinkType } from '../enums/social-link-type.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSocialLinkDto {
+    @ApiProperty()
     @IsEnum(SocialLinkType)
     type!: SocialLinkType;
 
+    @ApiProperty()
     @IsString()
     url!: string;
 }
