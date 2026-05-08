9ef1af19a9885624a4a5af2f2b6b357c5df5b4e6
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-08 17:11:38 -0300

    feat: add read methods to user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user-dto';
 import * as bcrypt from 'bcrypt';
@@ -25,5 +25,17 @@ export class UserService {
                 updatedAt: true,
             },
         });
+    }
+
+    async findAll() {
+        return await this.prisma.user.findMany();
+    }
+
+    async findOne(id: number) {
+        const user = this.prisma.user.findFirst({ where: { id } });
+
+        if (!user) throw new NotFoundException();
+
+        return user;
     }
 }
