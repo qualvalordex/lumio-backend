4875f371bc7540246851aba65af6b8b69dc6c02f
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-08 16:59:40 -0300

    feat: add prisma service to create user in db

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from '@nestjs/common';
+import { User } from 'generated/prisma';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user-dto';
 
 @Injectable()
 export class UserService {
-    constructor() {}
+    constructor(private readonly prisma: PrismaService) {}
 
-    async create() {
-        return { success: true }
+    async create(dto: CreateUserDto) {
+        const user = await this.prisma.user.findFirst({ where: { email: dto.email } });
+
+        if (user) throw new ConflictException();
+
+        return this.prisma.user.create({
+            data: dto,
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                isActive: true,
+                createdAt: true,
+                updatedAt: true,
+            },
+        });
     }
 }
