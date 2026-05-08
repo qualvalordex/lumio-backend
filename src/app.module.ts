bb145b4625c52710ba5d79ea2c5f0b61bac95672
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-08 16:39:47 -0300

    feat: add prisma module to manage db connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     UserModule,
   ],
   controllers: [],
