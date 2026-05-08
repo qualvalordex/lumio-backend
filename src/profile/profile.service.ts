ba6ae1e0eb2ee19d68a748a7dbc8b6bd3d77fbbc
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-08 19:05:57 -0300

    feat: add profile service

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,7 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Injectable()
 export class ProfileService {
     constructor(private readonly prisma: PrismaService) {}
+
+    async create(userId: number, dto: CreateProfileDto) {
+        const user = await this.prisma.user.findUnique({ where: { id: userId } });
+
+        if (!user) throw new NotFoundException();
+
+        return this.prisma.profile.create({
+            data: {
+                ...dto,
+                userId,
+            },
+        });
+    }
+
+    async update(userId: number, dto: CreateProfileDto) {
+        const profile = await this.prisma.profile.findUnique({ where: { userId: userId } });
+
+        if (!profile) throw new NotFoundException();
+
+        return this.prisma.profile.update({
+            where: { userId },
+            data: dto,
+        });
+    }
+
+    async findByUserId(userId: number) {
+        const profile = await this.prisma.profile.findUnique({ where: { userId } });
+
+        if (!profile) throw new NotFoundException();
+
+        return profile;
+    }
 }
