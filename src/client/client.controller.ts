04e2a52922639fe4b4de6a15834c3c09b640c833
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-10 21:58:25 -0300

    feat: add create route to client controller

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ClientService } from './client.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user';
+import { type AuthenticatedUser } from 'src/auth/types/authenticated-user';
+import { CreateClientDto } from './dto/create-client.dto';
 
 @ApiTags('Client')
+@ApiBearerAuth('access-token')
 @UseGuards(JwtAuthGuard)
 @Controller('client')
 export class ClientController {
     constructor(private readonly clientService: ClientService) {}
+
+    @ApiOperation({
+        summary: 'Criar um cliente',
+        description: 'Cria um cliente na carteira do usuário',
+    })
+    @Post()
+    async create(@CurrentUser() user: AuthenticatedUser, @Body() dto: CreateClientDto) {
+        return this.clientService.create(user.id, dto);
+    }
 }
