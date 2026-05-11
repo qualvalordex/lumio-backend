c8211ae734e3da0e57e1dc8d6e6e074f2f4a560e
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-10 21:19:58 -0300

    feat: add create client dto

---
+++ src/client/dto/create-client.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateClientDto {
+    @ApiProperty()
+    @IsString()
+    name!: string;
+
+    @ApiProperty()
+    @IsString()
+    phone!: string;
+
+    @ApiProperty()
+    @IsString()
+    cpfCnpj?: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    email?: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    notes?: string;
+}
