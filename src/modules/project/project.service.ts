0af9baa755342c45f6caeae17571e925a30218fc
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-13 11:19:36 -0300

    feat: add project creation method

--- src/modules/project/project.service.ts
+++ src/modules/project/project.service.ts
@@ -1,7 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @Injectable()
 export class ProjectService {
     constructor(private readonly prisma: PrismaService) {}
+
+    async create(userId: number, dto: CreateProjectDto) {
+        const user = await this.prisma.user.findUnique({ where: { id: userId } });
+        if (!user) throw new NotFoundException();
+
+        if (dto.clientId) {
+            const client = await this.prisma.client.findUnique({ where: { id: dto.clientId } });
+            if (!client) throw new NotFoundException();
+        }
+
+        const project = await this.prisma.project.create({
+            data: {
+                ...dto,
+                userId,
+            },
+        });
+
+        return project;
+    }
 }
