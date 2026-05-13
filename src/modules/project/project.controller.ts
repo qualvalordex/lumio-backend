4398bd6f937599ce796c7aa1c71f3958c1d173dd
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-13 11:28:24 -0300

    feat: add update project route

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { ProjectService } from './project.service';
 import { CurrentUser } from '../auth/decorators/current-user';
 import { type AuthenticatedUser } from '../auth/types/authenticated-user';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @ApiTags('Projects')
 @ApiBearerAuth('access-token')
@@ -20,5 +21,18 @@ export class ProjectController {
     @Post()
     async create(@CurrentUser() user: AuthenticatedUser, @Body() dto: CreateProjectDto) {
         return this.projectService.create(user.id, dto);
+    }
+
+    @ApiOperation({
+        summary: 'Atualizar projeto',
+        description: 'Atualiza o projeto do usuário.',
+    })
+    @Patch(':projectId')
+    async update(
+        @CurrentUser() user: AuthenticatedUser,
+        @Param('projectId', ParseIntPipe) projectId: number,
+        @Body() dto: UpdateProjectDto,
+    ) {
+        return this.projectService.update(user.id, projectId, dto);
     }
 }
