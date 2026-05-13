4c9dea452c4dffdac75c4ba82da2e2c8bf9ebbbd
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2026-05-13 11:19:46 -0300

    feat: add project creation route

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { ProjectService } from './project.service';
+import { CurrentUser } from '../auth/decorators/current-user';
+import { type AuthenticatedUser } from '../auth/types/authenticated-user';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @ApiTags('Projects')
 @ApiBearerAuth('access-token')
@@ -9,4 +12,13 @@ import { ProjectService } from './project.service';
 @Controller('project')
 export class ProjectController {
     constructor(private readonly projectService: ProjectService) {}
+
+    @ApiOperation({
+        summary: 'Criar projeto',
+        description: 'Cria um projeto novo para o usuário.',
+    })
+    @Post()
+    async create(@CurrentUser() user: AuthenticatedUser, @Body() dto: CreateProjectDto) {
+        return this.projectService.create(user.id, dto);
+    }
 }
